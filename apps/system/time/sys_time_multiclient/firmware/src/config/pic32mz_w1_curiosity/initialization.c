8c9670a21d0ad7e09b246420bdbbc513970d3cc2
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-08-19 18:49:51 +0530

    [SYS_TIME] Regenerated demo for system_time_sys_time_multiclient_pic32mz_w1_curiosity
    MH3-41971
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/system/time/sys_time_multiclient/firmware/src/config/pic32mz_w1_curiosity/initialization.c
+++ apps/system/time/sys_time_multiclient/firmware/src/config/pic32mz_w1_curiosity/initialization.c
@@ -195,6 +195,26 @@ const SYS_TIME_INIT sysTimeInitData =
 // *****************************************************************************
 // *****************************************************************************
 
+/*******************************************************************************
+  Function:
+    void STDIO_BufferModeSet ( void )
+
+  Summary:
+    Sets the buffering mode for stdin and stdout
+
+  Remarks:
+ ********************************************************************************/
+static void STDIO_BufferModeSet(void)
+{
+
+    /* Make stdin unbuffered */
+    setbuf(stdin, NULL);
+
+    /* Make stdout unbuffered */
+    setbuf(stdout, NULL);
+}
+
+
 
 
 /*******************************************************************************
@@ -212,9 +232,13 @@ void SYS_Initialize ( void* data )
     /* Start out with interrupts disabled before configuring any modules */
     __builtin_disable_interrupts();
 
+    STDIO_BufferModeSet();
+
+
   
-    SYS_PMU_MLDO_TRIM();
     CLK_Initialize();
+	SYS_PMU_MLDO_TRIM();
+
     /* Configure Wait States */
     PRECONbits.PFMWS = 5;
 
