e018b73bd876b895ed5e2934b7555a2a85f59e25
Arpan Kumar <arpan.kumar@microchip.com>
2021-08-20 01:40:57 +0530

    [DRV_SPI] Regenerated demo for driver_spi_sync_spi_self_loopback_multi_client_pic32mz_w1_curiosity_freertos
    MH3-55894
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/driver/spi/sync/spi_self_loopback_multi_client/firmware/src/config/pic32mz_w1_curiosity_freertos/initialization.c
+++ apps/driver/spi/sync/spi_self_loopback_multi_client/firmware/src/config/pic32mz_w1_curiosity_freertos/initialization.c
@@ -160,7 +160,7 @@ const DRV_SPI_PLIB_INTERFACE drvSPI0PlibAPI = {
     .writeRead = (DRV_SPI_PLIB_WRITE_READ)SPI2_WriteRead,
 
     /* SPI PLIB Transfer Status function */
-    .isBusy = (DRV_SPI_PLIB_IS_BUSY)SPI2_IsBusy,
+    .isTransmitterBusy = (DRV_SPI_PLIB_TRANSMITTER_IS_BUSY)SPI2_IsTransmitterBusy,
 
     /* SPI PLIB Callback Register */
     .callbackRegister = (DRV_SPI_PLIB_CALLBACK_REGISTER)SPI2_CallbackRegister,
@@ -188,11 +188,6 @@ const DRV_SPI_INIT drvSPI0InitData =
     /* SPI Client Objects Pool */
     .clientObjPool = (uintptr_t)&drvSPI0ClientObjPool[0],
 
-    /* DMA Channel for Transmit */
-    .dmaChannelTransmit = SYS_DMA_CHANNEL_NONE,
-
-    /* DMA Channel for Receive */
-    .dmaChannelReceive  = SYS_DMA_CHANNEL_NONE,
 
 };
 
@@ -247,8 +242,8 @@ void SYS_Initialize ( void* data )
     __builtin_disable_interrupts();
 
   
-    CLK_Initialize();
-	SYS_PMU_MLDO_TRIM();
+    PMU_Initialize();
+	CLK_Initialize();
 
     /* Configure Wait States */
     PRECONbits.PFMWS = 5;
