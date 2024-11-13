c4db2d5ad47016d80881c9c88fc244a319bb6450
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-11-13 19:14:28 +0530

    [DRV_SPI] Regenerated demo for driver__spi__async__spi_self_loopback_multi_client__firmware__pic32mz_w1_curiosity.X
    MH3-103426

--- apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/pic32mz_w1_curiosity/interrupts.c
+++ apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/pic32mz_w1_curiosity/interrupts.c
@@ -76,12 +76,12 @@ void SPI2_TX_Handler (void);
 // Section: System Interrupt Vector definitions
 // *****************************************************************************
 // *****************************************************************************
-void __ISR(_SPI2_RX_VECTOR, ipl1SRS) SPI2_RX_Handler (void)
+void __attribute__((used)) __ISR(_SPI2_RX_VECTOR, ipl1SRS) SPI2_RX_Handler (void)
 {
     SPI2_RX_InterruptHandler();
 }
 
-void __ISR(_SPI2_TX_VECTOR, ipl1SRS) SPI2_TX_Handler (void)
+void __attribute__((used)) __ISR(_SPI2_TX_VECTOR, ipl1SRS) SPI2_TX_Handler (void)
 {
     SPI2_TX_InterruptHandler();
 }
