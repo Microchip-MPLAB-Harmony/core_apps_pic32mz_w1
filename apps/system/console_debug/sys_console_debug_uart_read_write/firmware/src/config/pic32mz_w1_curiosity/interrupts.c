bcedcac6b9cb643d723c052189e78c9932c542dc
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-11-13 19:14:35 +0530

    [SYS_CONSOLE_DEBUG] Regenerated demo for system__console_debug__sys_console_debug_uart_read_write__firmware__pic32mz_w1_curiosity.X
    MH3-103448

--- apps/system/console_debug/sys_console_debug_uart_read_write/firmware/src/config/pic32mz_w1_curiosity/interrupts.c
+++ apps/system/console_debug/sys_console_debug_uart_read_write/firmware/src/config/pic32mz_w1_curiosity/interrupts.c
@@ -77,17 +77,17 @@ void UART1_TX_Handler (void);
 // Section: System Interrupt Vector definitions
 // *****************************************************************************
 // *****************************************************************************
-void __ISR(_UART1_FAULT_VECTOR, ipl1SRS) UART1_FAULT_Handler (void)
+void __attribute__((used)) __ISR(_UART1_FAULT_VECTOR, ipl1SRS) UART1_FAULT_Handler (void)
 {
     UART1_FAULT_InterruptHandler();
 }
 
-void __ISR(_UART1_RX_VECTOR, ipl1SRS) UART1_RX_Handler (void)
+void __attribute__((used)) __ISR(_UART1_RX_VECTOR, ipl1SRS) UART1_RX_Handler (void)
 {
     UART1_RX_InterruptHandler();
 }
 
-void __ISR(_UART1_TX_VECTOR, ipl1SRS) UART1_TX_Handler (void)
+void __attribute__((used)) __ISR(_UART1_TX_VECTOR, ipl1SRS) UART1_TX_Handler (void)
 {
     UART1_TX_InterruptHandler();
 }
