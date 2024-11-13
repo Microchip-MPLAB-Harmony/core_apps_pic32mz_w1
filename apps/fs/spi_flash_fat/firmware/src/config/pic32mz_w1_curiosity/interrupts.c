97ed3e8256fba6cc24ff75247225c34aac9714de
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-11-13 19:14:32 +0530

    [SYS_FS] Regenerated demo for fs__spi_flash_fat__firmware__pic32mz_w1_curiosity.X
    MH3-103442

--- apps/fs/spi_flash_fat/firmware/src/config/pic32mz_w1_curiosity/interrupts.c
+++ apps/fs/spi_flash_fat/firmware/src/config/pic32mz_w1_curiosity/interrupts.c
@@ -76,12 +76,12 @@ void SPI1_TX_Handler (void);
 // Section: System Interrupt Vector definitions
 // *****************************************************************************
 // *****************************************************************************
-void __ISR(_SPI1_RX_VECTOR, ipl1SRS) SPI1_RX_Handler (void)
+void __attribute__((used)) __ISR(_SPI1_RX_VECTOR, ipl1SRS) SPI1_RX_Handler (void)
 {
     SPI1_RX_InterruptHandler();
 }
 
-void __ISR(_SPI1_TX_VECTOR, ipl1SRS) SPI1_TX_Handler (void)
+void __attribute__((used)) __ISR(_SPI1_TX_VECTOR, ipl1SRS) SPI1_TX_Handler (void)
 {
     SPI1_TX_InterruptHandler();
 }
