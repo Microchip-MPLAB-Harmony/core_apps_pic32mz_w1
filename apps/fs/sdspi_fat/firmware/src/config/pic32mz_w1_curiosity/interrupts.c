fc44bedfccbd4119bc932b67a617c6346a17cd77
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-11-13 19:16:36 +0530

    [SYS_FS] Regenerated demo for fs__sdspi_fat__firmware__pic32mz_w1_curiosity.X
    MH3-103438

--- apps/fs/sdspi_fat/firmware/src/config/pic32mz_w1_curiosity/interrupts.c
+++ apps/fs/sdspi_fat/firmware/src/config/pic32mz_w1_curiosity/interrupts.c
@@ -77,17 +77,17 @@ void SPI2_TX_Handler (void);
 // Section: System Interrupt Vector definitions
 // *****************************************************************************
 // *****************************************************************************
-void __ISR(_CORE_TIMER_VECTOR, ipl1SRS) CORE_TIMER_Handler (void)
+void __attribute__((used)) __ISR(_CORE_TIMER_VECTOR, ipl1SRS) CORE_TIMER_Handler (void)
 {
     CORE_TIMER_InterruptHandler();
 }
 
-void __ISR(_SPI2_RX_VECTOR, ipl1SRS) SPI2_RX_Handler (void)
+void __attribute__((used)) __ISR(_SPI2_RX_VECTOR, ipl1SRS) SPI2_RX_Handler (void)
 {
     SPI2_RX_InterruptHandler();
 }
 
-void __ISR(_SPI2_TX_VECTOR, ipl1SRS) SPI2_TX_Handler (void)
+void __attribute__((used)) __ISR(_SPI2_TX_VECTOR, ipl1SRS) SPI2_TX_Handler (void)
 {
     SPI2_TX_InterruptHandler();
 }
