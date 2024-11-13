0e323409139a2a3b0167bc728f45fa232cde7e45
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-11-13 19:17:14 +0530

    [SYS_FS] Regenerated demo for fs__sdspi_fat__firmware__pic32mz_w1_curiosity_freertos.X
    MH3-103440

--- apps/fs/sdspi_fat/firmware/src/config/pic32mz_w1_curiosity_freertos/definitions.h
+++ apps/fs/sdspi_fat/firmware/src/config/pic32mz_w1_curiosity_freertos/definitions.h
@@ -49,13 +49,13 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include "peripheral/coretimer/plib_coretimer.h"
+#include "bsp/bsp.h"
 #include "system/fs/sys_fs.h"
 #include "system/fs/sys_fs_media_manager.h"
 #include "system/fs/sys_fs_fat_interface.h"
 #include "system/fs/fat_fs/file_system/ff.h"
 #include "system/fs/fat_fs/file_system/ffconf.h"
 #include "system/fs/fat_fs/hardware_access/diskio.h"
-#include "bsp/bsp.h"
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/cache/plib_cache.h"
@@ -85,13 +85,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32MZ1025W104132"
-#define DEVICE_ARCH			 "MIPS"
-#define DEVICE_FAMILY		 "PIC32MZW"
-#define DEVICE_SERIES		 "PIC32MZW"
+#define DEVICE_NAME          "PIC32MZ1025W104132"
+#define DEVICE_ARCH          "MIPS"
+#define DEVICE_FAMILY        "PIC32MZW"
+#define DEVICE_SERIES        "PIC32MZW"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 200000000
+#define CPU_CLOCK_FREQUENCY 200000000U
 
 // *****************************************************************************
 // *****************************************************************************
