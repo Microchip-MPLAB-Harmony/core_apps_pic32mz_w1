0bb1c1abaf0ba881b478433e9b6ea811962ae329
Syam Nair <Syam.Nair@microchip.com>
2025-09-23 10:19:49 +0000

    [APP] Regenerated demo for fs__nvm_fat__firmware
    MH3C-334

--- apps/fs/nvm_fat/firmware/src/config/pic32mz_w1_curiosity/definitions.h
+++ apps/fs/nvm_fat/firmware/src/config/pic32mz_w1_curiosity/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -61,11 +61,11 @@
 #include "peripheral/cache/plib_cache.h"
 #include "peripheral/evic/plib_evic.h"
 #include "driver/memory/drv_memory.h"
-#include "peripheral/nvm/plib_nvm.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "peripheral/nvm/plib_nvm.h"
 #include "app.h"
 
 
